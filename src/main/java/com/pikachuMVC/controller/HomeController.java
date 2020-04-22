e397d95140526c607e98ae5d14feef265de2d767
paul <paul860513@gmail.com>
2020-04-22 15:49:07 +0800

    add cards

--- src/main/java/com/pikachuMVC/controller/HomeController.java
+++ src/main/java/com/pikachuMVC/controller/HomeController.java
@@ -1,14 +1,36 @@
 package com.pikachuMVC.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.pikachuMVC.dao.CardDao;
+import com.pikachuMVC.model.CardBean;
+import com.pikachuMVC.service.CardService;
+
 @Controller
 public class HomeController {
-	public HomeController() {}
-	
-	@GetMapping({"/","/index"})
+	public HomeController() {
+	}
+
+	@Autowired
+	CardService service;
+
+	@Autowired
+	CardDao dao;
+
+	private List<CardBean> list = new ArrayList<CardBean>();
+
+	@GetMapping({ "/", "/index" })
 	public String home() {
+		if (list.size() == 0) {
+			dao.insertCards();
+			list = service.getCards();
+		}
+
 		return "index";
 	}
 }
