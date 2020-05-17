2b0ca88dc0ca5816846f0cb53a7dfe19d7083422
wade50110 <a0917940302@gmail.com>
2020-05-17 18:54:30 +0800

    家一些ajax

--- src/main/java/com/pikachuMVC/config/RootAppConfig.java
+++ src/main/java/com/pikachuMVC/config/RootAppConfig.java
@@ -33,8 +33,8 @@ public class RootAppConfig {
 				+ "&serverTimezone=Asia/Taipei");
 		ds.setUser("root");
 
-		ds.setPassword("404631060");
-//		ds.setPassword("22651330");
+//		ds.setPassword("404631060");
+		ds.setPassword("22651330");
 //		ds.setPassword("songsql");
 //		ds.setPassword("ruby123");	
 //		ds.setPassword("qazwsx12");	
--- src/main/webapp/shopping/js/shopping.js
+++ src/main/webapp/shopping/js/shopping.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
     $(".test").click(function(){
         alert("已加入購物車");
+    	$.ajax({
+			url: '/pikachuMVC/shopping/buyProductAjax',
+			type: 'post',
+			data: { productId: $(this).attr("productId"),
+					qty: $(this).attr("qty")
+			},
+			dataType: "html",
+			success:function(response){
+				console.log("55");
+			}
+
+		});
     })
-}
-)
+})
+
 
