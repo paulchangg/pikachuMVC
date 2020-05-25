84211c8b5d892f9b3533ba141666908bcbc2ff1b
james <james96317@gmail.com>
2020-05-25 11:01:01 +0800

    防止東西不見

--- src/main/java/com/pikachuMVC/controller/HomeController.java
+++ src/main/java/com/pikachuMVC/controller/HomeController.java
@@ -89,9 +89,9 @@ public class HomeController {
 	public String home() throws FailingHttpStatusCodeException, MalformedURLException, IOException, ParseException {
 		
 
-		if (!judgeNewsFolder()) {
-			newsService.newsCrawler();
-		}
+//		if (!judgeNewsFolder()) {
+//			newsService.newsCrawler();
+//		}
 
 		
 		if (list.size() == 0) {
